96b4324dd92986d46490789500760b163a3c42dc
Justproy <t.suthada@gmail.com>
2021-07-26 17:15:33 +0700

    fix: add async

--- backend/src/routes/hospitel.ts
+++ backend/src/routes/hospitel.ts
@@ -2,13 +2,15 @@ import express from 'express'
 import { getHostpitelbyID, getHostpitels } from '../hospitel/get'
 import { setHostpitel } from '../hospitel/set'
 const router = express.Router()
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
 
 router.get('/', async (req, res) => {
   const resp = await getHostpitels()
   res.status(200).send(resp)
 })
-router.post('/', (req, res) => {
-  const newHospitel = setHostpitel(req)
+router.post('/', async (req, res) => {
+  const newHospitel = await setHostpitel(req)
   res.status(200).json(newHospitel)
 })
 
